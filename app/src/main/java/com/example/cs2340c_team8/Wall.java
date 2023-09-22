4a770faa9875bd7bd2f3d37389623a3e331c492e
abjoshipura <2004akshara@gmail.com>
2023-09-22 13:29:06 -0400

    Create a basic dungeon layout

---
+++ app/src/main/java/com/example/cs2340c_team8/Wall.java
@@ -0,0 +1,72 @@
+package com.example.cs2340c_team8;
+
+public class Wall {
+    private float x, y;
+    private int height;
+    private int width;
+    private Wall[] neighbors;
+
+    public Wall(float x, float y, int height, int width, Wall[] neighbors) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+        this.neighbors = neighbors;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public Wall getLeftNeighbor() {
+        return neighbors[0];
+    }
+
+    public Wall getFrontNeighbor() {
+        return neighbors[1];
+    }
+
+    public Wall getRightNeighbor() {
+        return neighbors[2];
+    }
+
+    public Wall getBackNeighbor() {
+        return neighbors[3];
+    }
+
+    public void setLeftNeighbor(Wall wall) {
+        neighbors[0] = wall;
+    }
+
+    public void setFrontNeighbor(Wall wall) {
+        neighbors[1] = wall;
+    }
+
+    public void setRightNeighbor(Wall wall) {
+        neighbors[2] = wall;
+    }
+
+    public void setBackNeighbor(Wall wall) {
+        neighbors[3] = wall;
+    }
+}
---
+++ app/src/main/java/com/example/cs2340c_team8/WallView.java
@@ -0,0 +1,28 @@
+package com.example.cs2340c_team8;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+public class WallView extends View {
+    private Paint paint;
+    private Wall wall;
+
+    public WallView(Context context, Wall wall) {
+        super(context);
+
+        this.wall = wall;
+        paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        paint.setColor(Color.argb(255, 192, 194, 201));
+        canvas.drawRect(wall.getX(), wall.getY(), wall.getX() + wall.getWidth(),
+                wall.getY() + wall.getHeight(), paint);
+    }
+}
