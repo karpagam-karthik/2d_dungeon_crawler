0a301d7c01219ab5ae7495a873ffc818333d06b2
karpagam5789 <karpagam.k9999@gmail.com>
2023-10-04 23:04:42 -0400

    Create a singleton Player and related Classes

---
+++ app/src/main/java/com/example/cs2340c_team8/Consumable.java
@@ -0,0 +1,9 @@
+package com.example.cs2340c_team8;
+
+public interface Consumable {
+    int getQuantity();
+    String getEffect();
+    int getDuration();
+    // Add other methods or attributes related to consumables
+}
+
---
+++ app/src/main/java/com/example/cs2340c_team8/PowerUpType.java
@@ -0,0 +1,7 @@
+package com.example.cs2340c_team8;
+
+public enum PowerUpType {
+    HEALTH_BOOST,
+    TIME_EXTENSION,
+    // Add other power-up types as needed
+}
