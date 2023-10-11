dc345ad5c70c6b633896f5caf286afec48ce2b44
tahsinnabi <tahsin@nabi.us>
2023-10-11 16:00:50 -0400

    small changes to singleton

--- app/src/test/java/com/example/cs2340c_team8/SingletonTests.java
+++ app/src/test/java/com/example/cs2340c_team8/SingletonTests.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 public class SingletonTests {
     @Test
@@ -14,8 +13,29 @@ public class SingletonTests {
         // singleton to avoid errors with multiple leaderboards
         //checks constructor is private
         try {
-            Class test = Class.forName("com.example.cs2340c_team8.models.Leaderboard");
+            Class test = Class.forName("Leaderboard");
             Constructor<LeaderboardActivity>[] constructors = test.getDeclaredConstructors();
+            if (constructors.length != 1 || !constructors[0].isAccessible()) {
+                throw new ClassNotFoundException();
+            }
+            //check private instance
+            test.getField("instance");
+            Object test1 = constructors[0].newInstance();
+            Object test2 = constructors[0].newInstance();
+            assertTrue("Singletons cannot have more than one instance.", test1 == test2);
+        } catch (Exception e) {
+            assertFalse("Singletons must have private constructors, " +
+                    "public static getInstance(), and private static instances",false);
+        }
+    }
+    @Test
+    public void testPlayerSingleton() {
+        //Tahsin's unit test 2, make sure the player is a
+        // singleton to avoid errors with multiple leaderboards
+        //checks constructor is private
+        try {
+            Class test = Class.forName("Player");
+            Constructor<Player>[] constructors = test.getDeclaredConstructors();
             if (constructors.length != 1 || !constructors[0].isAccessible()) {
                 throw new ClassNotFoundException();
             }
