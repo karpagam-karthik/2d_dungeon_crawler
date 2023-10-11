88dfaeb255fc26ddadae0bca0894c4b225c63586
AidanSlater <aidanslater2@gmail.com>
2023-10-11 00:42:24 -0400

    Created game screen with starting rooms

--- app/src/main/java/com/example/cs2340c_team8/GameStartActivity.java
+++ app/src/main/java/com/example/cs2340c_team8/GameStartActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class GameStartActivity extends AppCompatActivity {
+    private int currImg = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,25 @@ public class GameStartActivity extends AppCompatActivity {
             exit.addCategory(Intent.CATEGORY_HOME);
             exit.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(exit);
+        });
+
+        ImageView startScreenMap = findViewById(R.id.mapOne);
+        Button tempChangeMap = findViewById(R.id.tempNextButton);
+
+        tempChangeMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View tempView) {
+                if (currImg == 1) {
+                    startScreenMap.setImageResource(R.drawable.map_two);
+                    currImg = 2;
+                } else if (currImg == 2) {
+                    startScreenMap.setImageResource(R.drawable.map_three);
+                    currImg = 3;
+                } else if (currImg == 3) {
+                    startScreenMap.setImageResource(R.drawable.map_one);
+                    currImg = 1;
+                }
+            }
         });
     }
 }
