c63447d1db8274bcbc9601a9d9a252a3f29c21d1
FishWithLegs9161 <tjn92948@uga.edu>
2023-10-22 13:06:43 -0400

    adding testcase descriptions

---
+++ app/src/main/java/com/example/cs2340c_team8/GameOverLayout.java
@@ -0,0 +1,4 @@
+package com.example.cs2340c_team8;
+
+public class GameOverLayout {
+}
---
+++ app/src/test/java/com/example/cs2340c_team8/testCases.java
@@ -0,0 +1,54 @@
+package com.example.cs2340c_team8;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import com.example.cs2340c_team8.models.Leaderboard;
+
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+
+public class testCases {
+    //test game ending scenario where the player wins,
+    //test if a player moves off screen or out of bounds what happens
+    //test putting in a date which has not occured as a score date
+    //test walls have no gaps in between them, neighbors list is full
+    //test getting to next room
+    //test gameover leaderboard scenario where a player has a negative score or two duplicate scores
+    //test players movement is strategy pattern
+    //test players movement is observer pattern
+    //test final exit exists/level
+    //test directiosn of player movement work as intended
+    @Test
+    public void testPlayerOutOfBounds() {
+    }
+    @Test
+    public void testNoWallGaps() {
+    }
+    @Test
+    public void testNextLevel() {
+    }
+    @Test
+    public void testPlayerStrategy() {
+    }
+    @Test
+    public void testPlayerObserver() {
+    }
+    @Test
+    public void testGameOverNegativeNDuplicate() {
+    }
+    @Test
+    public void testFinalLevel() {
+    }
+    @Test
+    public void testPlayerWins() {
+    }
+    @Test
+    public void testFutureDate() {
+    }
+    @Test
+    public void testPlayerDirection() {
+    }
+
+
+}
