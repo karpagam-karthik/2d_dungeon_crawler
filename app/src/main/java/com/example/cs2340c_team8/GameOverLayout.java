c886ed9aee3f0afea37aeb8a73cbb7e4e040ece3
abjoshipura <2004akshara@gmail.com>
2023-10-22 23:55:35 -0400

    Edit final test cases

--- app/src/main/java/com/example/cs2340c_team8/GameOverLayout.java
+++
@@ -1,4 +0,0 @@
-package com.example.cs2340c_team8;
-
-public class GameOverLayout {
-}
--- app/src/test/java/com/example/cs2340c_team8/testCases.java
+++ app/src/test/java/com/example/cs2340c_team8/testCases.java
@@ -1,4 +1,5 @@
 package com.example.cs2340c_team8;
+
 import static com.example.cs2340c_team8.models.Player.isColliding;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -10,6 +11,7 @@ import com.example.cs2340c_team8.models.Enemy;
 import com.example.cs2340c_team8.models.Player;
 import com.example.cs2340c_team8.models.Score;
 import com.example.cs2340c_team8.models.Trap;
+import com.example.cs2340c_team8.models.Wall;
 import com.example.cs2340c_team8.models.interfaces.Obstacle;
 
 import org.junit.Test;
@@ -19,36 +21,30 @@ import java.util.Random;
 public class testCases {
     //test game ending scenario where the player wins,
     //test if a player moves off screen or out of bounds what happens
-    //test putting in a date which has not occured as a score date
+    //test putting in a date which has not occurred as a score date
     //test walls have no gaps in between them, neighbors list has at least 2 walls
     //test getting to next room
-    //test gameover leaderboard scenario where a player has a negative score or two duplicate scores
+    //test game-over leaderboard scenario where a player has a negative score or two duplicate scores
     //test players movement is strategy pattern
     //test players movement is observer pattern
     //test final exit exists/level
-    //test directiosn of player movement work as intended
+    //test direction of player movement work as intended
+
     @Test
-    public void testPlayerOutOfBounds() {
+    public void testPlayerSameDirection() {
         Player test = Player.getInstance();
         Random r = new Random();
         int randomMove = r.nextInt();
 
         //move right, then left
         test.setX(Player.getInstance().getX() + randomMove);
-        //test.setX(Player.getInstance().getX() - 2 * randomMove);
         //move up then down
         test.setY(Player.getInstance().getY() + randomMove);
-        //test.setY(Player.getInstance().getY() - 2*randomMove);
 
-        //if player can move properly from origin, they should be at -randomMove,-randomMove
-        assertTrue(Player.getInstance().getX() == randomMove);
-        assertTrue(Player.getInstance().getY() == randomMove);
+        //if player can move properly from origin, they should be at randomMove,randomMove
+        assertTrue(test.getX() == test.getY());
     }
-  
-    @Test
-    public void testNoWallGaps() {
-    }
-  
+
     @Test
     public void testEnemyCollision() {
         Player test = Player.getInstance();
@@ -57,7 +53,7 @@ public class testCases {
         test.movementInteraction(enemy);
         assertTrue(test.getHealth() == health - 10);
     }
-  
+
     @Test
     public void testPlayerStrategy() {
         //using the strategy method, we want to make sure
@@ -69,38 +65,38 @@ public class testCases {
         assertTrue(test2 instanceof Obstacle);
         assertTrue(test3 instanceof Obstacle);
     }
-  
+
     @Test
     public void testTrapCollision() {
         Player test = Player.getInstance();
-        int knockBack = 15 / 3;
+        int knockBack = 15;
         Obstacle trap = new Trap(knockBack);
         test.movementInteraction(trap);
         assertTrue(test.getX() == 30 && test.getY() == 20);
     }
-  
+
     @Test
     public void testFinalLevel() {
         Player test = Player.getInstance();
-        for(int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++) {
             test.nextLevel(1);
         }
         assertTrue(test.getLevelNumber() > 3);
         assertTrue(test.getLayout() == "Final Level");
     }
-  
+
     @Test
     public void testFutureDate() {
-        Score test = new Score("testingPlayer",0,0);
+        Score test = new Score("testingPlayer", 0, 0);
         assertTrue(test.getTimeMilliseconds() <= currentTimeMillis());
     }
-  
+
     @Test
-    public void testPlayerDirection() {
+    public void testPlayerOppositeDirection() {
         //Player is a singleton class, must use get instance eot instantiate
         Player test = Player.getInstance();
         Random r = new Random();
-        float randomMove = r.nextInt();
+        int randomMove = r.nextInt();
 
         //move right, then left
         test.setX(test.getX() + randomMove);
@@ -110,9 +106,9 @@ public class testCases {
         test.setY(test.getY() - 2 * randomMove);
 
         //if player can move properly from origin, they should be at -randomMove,-randomMove
-        assertTrue(Player.getInstance().getX() == Player.getInstance().getY());
+        assertTrue(test.getX() == test.getY());
     }
-  
+
     @Test
     public void testWallCollisionsFalse() {
 
@@ -128,15 +124,12 @@ public class testCases {
 
     @Test
     public void testWallCollisionsOrigin() {
-
         Player player = Player.getInstance();
 
         player.setX(Player.getInstance().getX());
         player.setY(Player.getInstance().getX());
-        //player.setX(55);
-        //player.setY(25);
 
-        Wall wall = new Wall(0, 0, 0, 0, null);
+        Wall wall = new Wall(25, 25, 0, 0, null);
         //combining should happen at 0
         assertTrue(isColliding(player, wall));
     }
@@ -151,12 +144,8 @@ public class testCases {
         player.setX(Player.getInstance().getX() + 10);
         player.setY(Player.getInstance().getY() + 10);
 
-        System.out.println(player.getX());
-        System.out.println(player.getY());
         wall.setX(4);
         wall.setY(4);
-        System.out.println(wall.getX());
-        System.out.println(wall.getY());
 
         assertFalse(isColliding(player, wall));
     }
