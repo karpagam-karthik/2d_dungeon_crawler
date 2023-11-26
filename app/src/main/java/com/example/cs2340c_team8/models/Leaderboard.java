975411e56d1d4b700e2d10dc5788f2cdbf2f6c0e
karpagam5789 <129133364+karpagam5789@users.noreply.github.com>
2023-11-26 11:52:41 -0500

    Add Comments to Leaderboard.java

--- app/src/main/java/com/example/cs2340c_team8/models/Leaderboard.java
+++ app/src/main/java/com/example/cs2340c_team8/models/Leaderboard.java
@@ -1,19 +1,29 @@
+// Package declaration
 package com.example.cs2340c_team8.models;
 
+// Import statements for required Java classes
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+// Declaration of the 'Leaderboard' class
 public class Leaderboard {
+    // Singleton instance of the 'Leaderboard' class
     private static volatile Leaderboard leaderboard;
+
+    // Root node of the leaderboard's binary search tree
     private LeaderboardNode root;
+
+    // Number of scores in the leaderboard
     private int size;
 
+    // Private constructor to ensure singleton pattern
     private Leaderboard() {
         root = null;
         size = 0;
     }
 
+    // Method to get the singleton instance of the leaderboard
     public static Leaderboard getInstance() {
         if (leaderboard == null) {
             synchronized (Leaderboard.class) {
@@ -26,6 +36,7 @@ public class Leaderboard {
         return leaderboard;
     }
 
+    // Method to add a score to the leaderboard
     public void addScore(Score score) {
         if (score == null) {
             throw new IllegalArgumentException();
@@ -33,6 +44,7 @@ public class Leaderboard {
         root = addScoreH(root, score);
     }
 
+    // Helper method to recursively add a score to the leaderboard
     private LeaderboardNode addScoreH(LeaderboardNode node, Score score) {
         if (node == null) {
             size++;
@@ -48,12 +60,14 @@ public class Leaderboard {
         }
     }
 
+    // Method to get the top N scores from the leaderboard
     public List<Score> getTopNScores(int limit) {
         ArrayList<Score> list = new ArrayList<>(size);
         getTopNScoresH(list, root, limit);
         return list;
     }
 
+    // Helper method to recursively get the top N scores from the leaderboard
     private void getTopNScoresH(ArrayList<Score> list, LeaderboardNode node, int limit) {
         if (node != null && list.size() < limit) {
             getTopNScoresH(list, node.getRight(), limit);
@@ -62,6 +76,7 @@ public class Leaderboard {
         }
     }
 
+    // Method to get the top score from the leaderboard
     public Score getTopScore() {
         if (size == 0) {
             throw new NoSuchElementException();
@@ -70,6 +85,7 @@ public class Leaderboard {
         return getTopNScores(1).get(0);
     }
 
+    // Static method to compare two scores for sorting
     public static long compareScores(Score score1, Score score2) {
         if (score1.getScore() - score2.getScore() == 0) {
             return score2.getTimeMilliseconds() - score1.getTimeMilliseconds();
@@ -77,10 +93,12 @@ public class Leaderboard {
         return score1.getScore() - score2.getScore();
     }
 
+    // Method to get the root node of the leaderboard's binary search tree
     public LeaderboardNode getRoot() {
         return root;
     }
 
+    // Method to get the number of scores in the leaderboard
     public int getSize() {
         return size;
     }
