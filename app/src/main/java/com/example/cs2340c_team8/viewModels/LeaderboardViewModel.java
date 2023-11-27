d6b2aab6f7dcbc941ba77220d7180073630173cb
tahsinnabi <tahsin@nabi.us>
2023-11-26 23:52:37 -0500

    added base for decorator powerups

--- app/src/main/java/com/example/cs2340c_team8/viewModels/LeaderboardViewModel.java
+++ app/src/main/java/com/example/cs2340c_team8/viewModels/LeaderboardViewModel.java
@@ -2,13 +2,10 @@ package com.example.cs2340c_team8.viewModels;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.view.View;
 
 import androidx.databinding.BaseObservable;
 
-import com.example.cs2340c_team8.R;
-import com.example.cs2340c_team8.models.GameConfig;
 import com.example.cs2340c_team8.models.Leaderboard;
 import com.example.cs2340c_team8.models.Score;
 import com.example.cs2340c_team8.views.activities.CreditsActivity;
@@ -17,7 +14,7 @@ import com.example.cs2340c_team8.views.activities.TitleActivity;
 import java.util.List;
 
 public class LeaderboardViewModel extends BaseObservable {
-    private static final Leaderboard LEADERBOARD = Leaderboard.getInstance();
+    private static final Leaderboard leaderboard = Leaderboard.getInstance();
     private final Activity activity;
     private final int score;
     private final String time;
@@ -30,32 +27,14 @@ public class LeaderboardViewModel extends BaseObservable {
         this.time = activity.getIntent().getStringExtra("time");
         this.keys = activity.getIntent().getStringExtra("keys");
         this.success = activity.getIntent().getBooleanExtra("success", false);
-
-        GameConfig.getMainThemePlayer().pause();
-
-        if (success) {
-            GameConfig.setLevelPlayer(MediaPlayer.create(activity, R.raw.won));
-        } else {
-            GameConfig.setLevelPlayer(MediaPlayer.create(activity, R.raw.lose));
-        }
-        GameConfig.getLevelPlayer().start();
-        GameConfig.getLevelPlayer().setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                GameConfig.getMainThemePlayer().seekTo(0);
-                GameConfig.getMainThemePlayer().setVolume(1, 1);
-                GameConfig.getMainThemePlayer().setLooping(true);
-                GameConfig.getMainThemePlayer().start();
-            }
-        });
     }
 
     public static void addNewScore(String username, int score, long time) {
-        LEADERBOARD.addScore(new Score(username, score, time));
+        leaderboard.addScore(new Score(username, score, time));
     }
 
     public Score getNthTopScore(int n) {
-        List<Score> topScores = LEADERBOARD.getTopNScores(5);
+        List<Score> topScores = leaderboard.getTopNScores(5);
         if (n >= topScores.size()) {
             return null;
         }
@@ -79,10 +58,6 @@ public class LeaderboardViewModel extends BaseObservable {
     }
 
     public void playAgain(View view) {
-        GameConfig.getMainThemePlayer().pause();
-
-        GameConfig.setLevel(1);
-
         Intent settings = new Intent(activity, TitleActivity.class);
         activity.startActivity(settings);
         activity.finish();
