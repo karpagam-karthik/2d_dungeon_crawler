b3e03d0fa31504520082c41bcab4579a1e0c21bf
tahsinnabi <tahsin@nabi.us>
2023-11-28 18:30:33 -0500

    added more test cases

--- app/src/test/java/com/example/cs2340c_team8/SprintFiveTestCases.java
+++ app/src/test/java/com/example/cs2340c_team8/SprintFiveTestCases.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import android.content.Context;
+import android.content.ContextWrapper;
+
 import com.example.cs2340c_team8.models.Enemy;
 import com.example.cs2340c_team8.models.GameConfig;
 import com.example.cs2340c_team8.models.Leaderboard;
@@ -25,6 +28,7 @@ import com.example.cs2340c_team8.views.GameView;
 import org.junit.Assert;
 import org.junit.Test;
 import java.lang.reflect.Constructor;
+import java.util.concurrent.TimeUnit;
 
 public class SprintFiveTestCases {
     @Test
@@ -52,25 +56,24 @@ public class SprintFiveTestCases {
         FirePowerUp target3 = new FirePowerUp(null, 100,100);
         test.setStartX(30);
         test.setStartY(40);
-        target3.attackPlayer();
-        boolean rtn = GameView.getFireballRange() >= 500;
-        assertTrue("The players fire attack did not increase", rtn);
+        target1.attackPlayer();
+        assertTrue("The players fire attack did not increase", test.getHealth() <= 200);
     }
 
     @Test
     public void testIceFlower() {
         GameConfig.setDifficulty(Difficulty.BEGINNER);
-        BulletBill target1 = new BulletBill(null, 25, 25, 0, 20);
-        Goomba target2 = new Goomba(null, 50, 50, false, 10);
+        BulletBill target1 = new BulletBill(null, 25, 25, 0, 0);
+        Goomba target3 = new Goomba(null, 50, 50, false, 0);
         Player test = Player.getInstance();
-        IcePowerUp target3 = new IcePowerUp(null, 100,100);
+        IcePowerUp target2 = new IcePowerUp(null, 100,100);
         test.setStartX(30);
         test.setStartY(40);
         int prev1 = target1.getMovementSpeed();
-        int prev2 = target2.getMovementSpeed();
+        int prev2 = target3.getMovementSpeed();
         target3.attackPlayer();
         boolean rtn1 = target1.getMovementSpeed() == prev1 / 2;
-        boolean rtn2 = target2.getMovementSpeed() == prev2 / 2;
+        boolean rtn2 = target3.getMovementSpeed() == prev2 / 2;
         assertTrue("The enemies did not get frozen", rtn1 && rtn2);
     }
 
@@ -84,13 +87,70 @@ public class SprintFiveTestCases {
         test.setStartX(30);
         test.setStartY(40);
         int healthPrev = test.getHealth();
-        target3.attackPlayer();
         target1.attackPlayer();
         target2.attackPlayer();
-        boolean rtn1 = test.getHealth() >= healthPrev;
+        boolean rtn1 = test.getHealth() <= healthPrev;
         assertTrue("The player did not get shielded", rtn1);
     }
 
+    @Test
+    public void TestStarPowerupDuration() {
+        GameConfig.setDifficulty(Difficulty.BEGINNER);
+        BulletBill target1 = new BulletBill(null, 25, 25, 0, 20);
+        Goomba target2 = new Goomba(null, 50, 50, false, 10);
+        Player test = Player.getInstance();
+        StarPowerUp target3 = new StarPowerUp(null, 100,100);
+        test.setStartX(30);
+        test.setStartY(40);
+        int healthPrev = test.getHealth();
+        boolean rtn1 = test.getHealth() == healthPrev;
+        target1.attackPlayer();
+        target2.attackPlayer();
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        assertTrue("The player returned to its og health", rtn1);
+    }
 
+    @Test
+    public void TestIcePowerupDuration() {
+        GameConfig.setDifficulty(Difficulty.BEGINNER);
+        BulletBill target1 = new BulletBill(null, 25, 25, 0, 20);
+        Goomba target3 = new Goomba(null, 50, 50, false, 10);
+        Player test = Player.getInstance();
+        IcePowerUp target2 = new IcePowerUp(null, 100,100);
+        test.setStartX(30);
+        test.setStartY(40);
+        int prev1 = target1.getMovementSpeed();
+        int prev2 = target3.getMovementSpeed();
+        target3.attackPlayer();
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        boolean rtn1 = target1.getMovementSpeed() == prev1 / 2;
+        boolean rtn2 = target3.getMovementSpeed() == prev2 / 2;
+        assertTrue("The enemies did not begin moving", !rtn1 && !rtn2);
+    }
+    @Test
+    public void TestFirePowerupDuration() {
+        GameConfig.setDifficulty(Difficulty.BEGINNER);
+        BulletBill target1 = new BulletBill(null, 25, 25, 0, 0);
+        Goomba target2 = new Goomba(null, 50, 50, false, 0);
+        Player test = Player.getInstance();
+        FirePowerUp target3 = new FirePowerUp(null, 100,100);
+        test.setStartX(30);
+        test.setStartY(40);
+        target1.attackPlayer();
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        assertTrue("The players fire attack did not return crease", test.getHealth() <= 400);
+    }
 
 }
